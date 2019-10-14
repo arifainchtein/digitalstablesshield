2055b4b69a9b140c63ee92f4e24dcb89f4173646
Ari Fainchtein <ari@digitalgeppetto.com>
2019-10-14 13:12:38 +1100

    working on flow meter events

---
+++ libraries/DigitalStables/FaultRecord.h
@@ -0,0 +1,9 @@
+/*
+ * This file is generated in the spermatogenesis process
+ */
+#ifndef FAULTRECORD_H
+ #define FAULTRECORD_H
+struct FaultRecord{
+	long startTime;
+};
+#endif
---
+++ libraries/DigitalStables/FlowMeterEventFault.h
@@ -0,0 +1,21 @@
+/*
+ * FlowMeterEventData.h
+ *
+ *  Created on: 6 May 2019
+ *      Author: arifainchtein
+ */
+
+#ifndef LIBRARIES_DIGITALSTABLES_FLOWMETEREVENTFAULT_H_
+#define LIBRARIES_DIGITALSTABLES_FLOWMETEREVENTFAULT_H_
+
+#include "Arduino.h"
+#include <FaultRecord.h>
+
+typedef struct FlowMeterEventFault:public FaultRecord{
+	float flow;
+	uint8_t faultCode;
+};
+
+
+
+#endif /* LIBRARIES_DIGITALSTABLES_FLOWMETEREVENTDATA_H_ */
