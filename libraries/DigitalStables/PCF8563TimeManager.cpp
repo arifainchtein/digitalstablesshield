6ca7ccc052be130031eab893cd434ac8220426b7
arifainchtein <ari@digitalgeppetto.com>
2021-03-22 11:26:43 +1100

    adding gloriafielfd v2 hardware

--- libraries/DigitalStables/PCF8563TimeManager.cpp
+++ libraries/DigitalStables/PCF8563TimeManager.cpp
@@ -270,7 +270,7 @@ String PCF8563TimeManager::getElapsedTimeHoursMinutesSecondsString(long elapsedT
 	// this sets the alarm data to the PCF8563
 	{
 	  byte am, ah, ad, adow;
-	  am = decToBcd(0); //alarmMinute
+	  am = decToBcd(28); //alarmMinute
 	  am = am | 100000000; // set minute enable bit to on
 	  ah = decToBcd(0);
 	  ah = ah | 100000000; // set hour enable bit to on
