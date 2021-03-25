8caf0dc141352e99a2e7e25ef1d94022987fd382
arifainchtein <ari@digitalgeppetto.com>
2021-03-25 11:30:48 +1100

    adding gloriafielfd v2 hardware

--- libraries/DigitalStables/PCF8563TimeManager.h
+++ libraries/DigitalStables/PCF8563TimeManager.h
@@ -40,7 +40,7 @@ public:
 	//
 	// oscillator functions
 	//
-	void PCF8563TimeManager::PCF8563oscOFF();
+	void PCF8563oscOFF();
 	void PCF8563osc1Hz();
 	void PCF8563osc32Hz();
 	void PCF8563osc1024kHz();
